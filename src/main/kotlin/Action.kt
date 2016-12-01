92af6b6385de51d28f496fec113f299478278d85
e.zaharov <devnull@localhost>
2016-12-02 00:48:16 +0300

    increase tree destroy range

--- src/main/kotlin/Action.kt
+++ src/main/kotlin/Action.kt
@@ -35,7 +35,7 @@ abstract class Action {
 
     open fun move(target: Any?): Boolean {
         val nearestTree = findHelper.getAllTrees()
-                .filter { tree -> abs(self.getAngleTo(tree)) < PI / 4 }
+                .filter { tree -> abs(self.getAngleTo(tree)) < PI / 3 }
                 .filter { tree -> self.getDistanceTo(tree) <= self.radius + tree.radius + MIN_CLOSEST_TREE_DISTANCE }
                 .firstOrNull()
 
@@ -180,7 +180,7 @@ abstract class Action {
         val MIN_BASE_DISTANCE_FACTOR = 0.5
 
         val MIN_CLOSEST_DISTANCE = 5.0
-        val MIN_CLOSEST_TREE_DISTANCE = 10.0
+        val MIN_CLOSEST_TREE_DISTANCE = 15.0
 
         val FETISH_CLOSE_MULTIPLIER = 1.2
         val ORC_CLOSE_MULTIPLIER = 3.0
