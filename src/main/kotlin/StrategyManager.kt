8fd452619ed95bc8e42d805c4b39d8ee4082b8cc
e.zaharov <devnull@localhost>
2016-12-13 10:49:06 +0300

    v70

--- src/main/kotlin/StrategyManager.kt
+++ src/main/kotlin/StrategyManager.kt
@@ -278,7 +278,7 @@ class StrategyManager {
 
         if (ifWizardOnAttackLine()) return currentLaneType
 
-        val laneToChoose = wizard.getMessages().firstOrNull()?.lane
+        val laneToChoose = wizard.getMessages().lastOrNull()?.lane
         if (laneToChoose != null)
             return laneToChoose
 
